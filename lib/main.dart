49756de3455bc41941b6d7cbdc995dff94decb2e
hamad112002 <hammadmanzoor968@gmail.com>
2024-09-07 04:23:24 -0700

    basics for designing ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar:  AppBar(
-        backgroundColor: Colors.amber,
-        title: const Text("My first Flutter Project"),
-      ),
-    body: const Center(
-      child: Image(image: AssetImage('assets/hamad.jpg'),
-      ),
-    ),
-    ),
-  ));
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    print('Hello world');
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent,
+          title:const Text("First Flutter Project ",style: TextStyle(fontSize: 30,fontFamily: 'Pacifo'),
+          ),
+        ),
+      body: Column(
+
+        children: [
+          Row(
+           children: [
+             Container(
+               child:  Text("First Cont"),
+               height: 100,
+               width: 100,
+               color:Colors.pinkAccent ,
+             ),
+             Container(
+               child:  Text("Second Cont"),
+               height: 100,
+               width: 100,
+               color:Colors.lightGreen ,
+             ),
+             Container(
+               child:  Text("Third Cont"),
+               height: 100,
+               width: 100,
+               color:Colors.deepOrangeAccent ,
+             ),
+      ]
+          ),
+          Container(
+            child:  Text("First Cont"),
+            height: 100,
+            width: 100,
+            color:Colors.amber ,
+          ),
+
+          Container(
+            child: Text("Second Cont"),
+            height: 100,
+            width: 100,
+            color:Colors.amber ,
+          ),
+
+          Container(
+
+            child: Text("Third Cont"),
+            height: 100,
+            width: 100,
+            color:Colors.amber ,
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
